531ee69afb43556aa135e63395d6ea0597314f58
bronik7 <b_z94102@yahoo.com>
2022-04-19 14:06:15 -0700

    TestHome class now inherits from BaseTest class

--- tests/system/test_home.py
+++ tests/system/test_home.py
@@ -1,9 +1,10 @@
-from unittest import TestCase
-from app import app
+from tests.system.base_test import BaseTest
 import json
-class TestHome(TestCase):
+
+
+class TestHome(BaseTest):
     def test_home(self):
-        with app.test_client() as c:
+        with self.app() as c:
             req = c.get("/")
-            self.assertEqual(req.status_code,200)
+            self.assertEqual(req.status_code, 200)
             self.assertEqual(json.loads(req.get_data()), {'message': "Hello World!"})
